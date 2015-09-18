670d02c46da71589db5b2c7181e44d8178d67c4b
Inan Tainwala <it15@duke.edu>
2015-09-17 21:07:58 -0400

    XML Parser Basic set up

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,23 +1,26 @@
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class XMLParser {
 
 
-
-
-	public void main(String argv[]) {
+	public static void main(String argv[])	throws ParserConfigurationException, SAXException, IOException {
 
 		try {
 
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			File fXmlFile = new File(getClass().getClassLoader().getResourceAsStream("file.xml"));
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
+			Document doc = dBuilder.parse((XMLParser.class.getResourceAsStream("config.xml")));
 
-			//optional, but recommended
-			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 			doc.getDocumentElement().normalize();
 
 			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
@@ -28,5 +31,24 @@ public class XMLParser {
 
 			for (int temp = 0; temp < nList.getLength(); temp++) {
 
+				Node nNode = nList.item(temp);
 
+				System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+
+					System.out.println("Staff id : " + eElement.getAttribute("id"));
+					System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+					System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+					System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+					System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
