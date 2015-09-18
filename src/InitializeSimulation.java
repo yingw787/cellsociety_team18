312c3e03d29216e59553958e98d69d6f8ffaebb9
Inan Tainwala <it15@duke.edu>
2015-09-18 16:18:11 -0400

    Renamed class to reflect behavior more accurately

--- src/InitializeSimulation.java
+++ src/InitializeSimulation.java
@@ -8,7 +8,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-public class XMLParser {
+public class InitializeSimulation {
 
 	private final static String[] XMLFileNames = {"schellingSegregation.xml", "waTor.xml", "spreadingFire.xml", "gameOfLife.xml"};
 	private final static String[] parserClassNames = {"XMLToSegregationDOMs", "XMLToWaTorDOMs", "XMLToSpreadingFireDOMs", "XMLToGameOfLifeDOMs.xml"};
@@ -22,7 +22,7 @@ public class XMLParser {
 		try {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse((XMLParser.class.getResourceAsStream(XMLFileNames[sim])));
+			Document doc = dBuilder.parse((InitializeSimulation.class.getResourceAsStream(XMLFileNames[sim])));
 			Constructor c = Class.forName(parserClassNames[sim]).getConstructor(Document.class);
 			dataTransfer = (XMLToDOM) c.newInstance(doc);
 			dataTransfer.createDOMfromXML();
