956596eb0555c573d3ddfec7f12e42d688c498fa
Ying Wang <yingw787@gmail.com>
2015-09-19 02:21:29 -0400

    Can run simulations using the media control box

--- src/Visualization.java
+++ src/Visualization.java
@@ -12,48 +12,74 @@ import javafx.scene.shape.Rectangle;
 public class Visualization extends GridPane {
 	
 	// the height of the visualization is pretty much whatever we decide it to be 
-	private static double VisualizationHeight;; 
+	private static double VisualizationHeight;
 	private static double VisualizationWidth; 
+	private Integer myNumberOfRows, myNumberOfColumns;
+	private GridPane myPane;
+	
+	private GridOfCells myGridOfCells; 
+	private Cell[][] my2DArrayOfCells;
+	
+	public Visualization(GridOfCells gridOfCells){
+		myGridOfCells = gridOfCells; 
+		my2DArrayOfCells = gridOfCells.getMyCells();
+		
+	}
 	
 	@SuppressWarnings("static-access")
-	public Scene init(int numberOfRows, int numberOfColumns, double visWidth, double visHeight){
+	public Scene init(double visWidth, double visHeight){
+		
+		myNumberOfRows = my2DArrayOfCells.length;
+		myNumberOfColumns = my2DArrayOfCells[0].length;
 		
 		VisualizationHeight = visHeight; 
 		VisualizationWidth = visWidth; 
 		
-		GridPane pane = new GridPane(); 
+		myPane = new GridPane(); 
 		
 		// set the number of rows 
-		for(int i = 0; i < numberOfRows; i++){
-			RowConstraints row = new RowConstraints(VisualizationHeight/numberOfRows);
-			pane.getRowConstraints().add(row);
+		for(int i = 0; i < myNumberOfRows; i++){
+			RowConstraints row = new RowConstraints(VisualizationHeight/myNumberOfRows);
+			myPane.getRowConstraints().add(row);
 		}
 		
-		for(int i = 0; i < numberOfColumns; i++){
-			ColumnConstraints column = new ColumnConstraints(VisualizationWidth/numberOfColumns);
-			pane.getColumnConstraints().add(column);
+		for(int i = 0; i < myNumberOfColumns; i++){
+			ColumnConstraints column = new ColumnConstraints(VisualizationWidth/myNumberOfColumns);
+			myPane.getColumnConstraints().add(column);
 		}
 		
-		for(int i = 0; i < numberOfRows; i++){
-			for(int j = 0; j < numberOfColumns; j++){
-				Rectangle tile = new Rectangle(VisualizationHeight/numberOfRows, VisualizationWidth/numberOfColumns);
-				tile.setFill(Color.RED);
-				pane.add(tile, i, j);
-
-			}
-		}
+		drawCells();
 		
-		pane.setGridLinesVisible(true);
+		myPane.setGridLinesVisible(true);
 		Group root = new Group(); 
-		root.getChildren().add(pane);
+		root.getChildren().add(myPane);
 		Scene scene = new Scene(root);
 		
 		
-		Rectangle testTile = getTile(2, 3, pane);
-		setColorOfRectangle(testTile, Color.BLUE);
+//		Rectangle testTile = getTile(2, 3, pane);
+//		setColorOfRectangle(testTile, Color.BLUE);
 		
 		return scene;
 		
+	}
+
+	public void drawCells() {
+		for(int i = 0; i < myNumberOfRows; i++){
+			for(int j = 0; j < myNumberOfColumns; j++){
+				Rectangle tile = new Rectangle(VisualizationHeight/myNumberOfRows, VisualizationWidth/myNumberOfColumns);
+				java.awt.Color awtColor = myGridOfCells.getCellColor(j, i) ;
+				System.out.println(j+" "+i);
+				int r = awtColor.getRed();
+				int g = awtColor.getGreen();
+				int b = awtColor.getBlue();
+				int a = awtColor.getAlpha();
+				double opacity = a / 255.0 ;
+				Color fxColor = Color.rgb(r, g, b, opacity);
+				tile.setFill(fxColor);
+				myPane.add(tile, i, j);
+
+			}
+		}
 	}
 	
 	
