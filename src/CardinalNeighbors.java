4ea332fbecf6798aae2b39ec78f18579b56d574d
John Dai <johnrundai@gmail.com>
2015-09-24 15:46:02 -0400

    fixed some duplicated code

--- src/CardinalNeighbors.java
+++ src/CardinalNeighbors.java
@@ -11,17 +11,14 @@ public class CardinalNeighbors extends NeighborProcessor{
                                                       int xmax,
                                                       int ymax,
                                                       ArrayList<ArrayList<Cell>> grid) {
-        int x,y;
-        Iterator<Pair<Integer,Integer>> i = neighborPoints.iterator();
-        while (i.hasNext()) {
-            Pair<Integer,Integer> p = i.next();
-            x=(int) p.getKey();
-            y=(int) p.getValue();
-            if (!(x==currentX || y==currentY)) {
-                i.remove();
-            }
-        }
-        return neighborPoints;
+        return defaultProcess(currentX,currentY,neighborPoints,xmax,ymax,grid);
+    }
+    @Override
+    public boolean checkCondition(int currentX, int currentY, int xmax, int ymax, int x, int y) {
+        return condition (currentX, currentY, x, y);
+    }
+    public boolean condition (int currentX, int currentY, int x, int y) {
+        return!(x==currentX || y==currentY);
     }
 
 }
--- src/NeighborProcessor.java
+++ src/NeighborProcessor.java
@@ -1,6 +1,28 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import javafx.util.Pair;
 
 public abstract class NeighborProcessor {
     public abstract ArrayList<Pair<Integer, Integer>> process(int currentX, int currentY, ArrayList<Pair<Integer,Integer>> neighborPoints, int xmax, int ymax, ArrayList<ArrayList<Cell>> grid);
+    public ArrayList<Pair<Integer, Integer>> defaultProcess (int currentX,
+                                                      int currentY,
+                                                      ArrayList<Pair<Integer, Integer>> neighborPoints,
+                                                      int xmax,
+                                                      int ymax,
+                                                      ArrayList<ArrayList<Cell>> grid) {
+        int x,y;
+        Iterator<Pair<Integer,Integer>> i = neighborPoints.iterator();
+        while (i.hasNext()) {
+            Pair<Integer,Integer> p = i.next();
+            x=(int) p.getKey();
+            y=(int) p.getValue();
+            if (checkCondition(currentX, currentY, xmax, ymax, x, y)) {
+                i.remove();
+            }
+        }
+        return neighborPoints;
+    }
+    public boolean checkCondition(int currentX, int currentY, int xmax, int ymax, int x, int y) {
+        return false;
+    };
 }
